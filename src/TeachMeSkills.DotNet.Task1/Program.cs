2b580d7314af6dc0b1256a9830f97759a1777e54
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-09-20 19:39:06 +0300

    feat: add solution for Task 1

--- src/TeachMeSkills.DotNet.Task1/Program.cs
+++ src/TeachMeSkills.DotNet.Task1/Program.cs
@@ -2,11 +2,47 @@
 
 namespace TeachMeSkills.DotNet.Task1
 {
+    class Rectangle
+    {
+        double side1;
+        double side2;
+        public Rectangle(double side1, double side2) { this.side1 = side1; this.side2 = side2; }
+        private double AreaCalculator()
+        {
+            return side1 * side2;
+        }
+        private double PerimeterCalculator()
+        {
+            return 2 * (side1 + side2);
+        }
+
+        public double Area
+        {
+            get
+            {
+                return AreaCalculator();
+            }
+        }
+        public double Perimeter
+        {
+            get
+            {
+                return PerimeterCalculator();
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            // Your code here..
+            Console.Write("Enter side 1 : ");
+            double side1 = Double.Parse(Console.ReadLine());
+            Console.Write("Enter side 2 : ");
+            double side2 = Double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Rectangle rectangle = new Rectangle(side1, side2);
+            Console.WriteLine("Perimeter : {0:00.00}", rectangle.Perimeter);
+            Console.WriteLine("Area : {0:00.00}", rectangle.Area);
 
             Console.ReadLine();
         }
