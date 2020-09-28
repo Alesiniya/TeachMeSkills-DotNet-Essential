f98c50873cded7886cc1858b26fb5eb68f9cf76f
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-09-29 00:07:20 +0300

    feat: Start solution Task 10

---
+++ src/TachMeSkills.DotNet.Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TachMeSkills.DotNet.Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
