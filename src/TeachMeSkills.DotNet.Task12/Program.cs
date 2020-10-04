52e66e17b0d35ffea18cfb46fffcea7920c3d01b
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-10-05 00:47:17 +0300

    feat: add solution for Task 12

---
+++ src/TeachMeSkills.DotNet.Task12/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task12
+{
+    public class Printer
+    {
+        public virtual void Print(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+    public class LaserPrinter : Printer
+    {
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(value);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+    public class InkPrinter : Printer
+    {
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(value);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer printer = new Printer();
+            printer.Print("Printer");
+            Console.WriteLine("-----");
+            LaserPrinter laserPrinter = new LaserPrinter();
+            printer = laserPrinter;
+            printer.Print("Laser  Printer.");
+            ((LaserPrinter)printer).Print("Cast printer");
+            Console.WriteLine("-----");
+            InkPrinter inkPrinter = new InkPrinter();
+            printer = inkPrinter;
+            printer.Print("Ink Printer.");
+            ((InkPrinter)printer).Print("Cast printer.");
+        }
+    }
+}
