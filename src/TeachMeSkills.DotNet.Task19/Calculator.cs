6863e5b46eb1c08574e203b84ceccfab57c037a6
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-10-10 22:47:53 +0300

    feat: add solution for Task 19

---
+++ src/TeachMeSkills.DotNet.Task19/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeachMeSkills.DotNet.Task19
+{
+    public static class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Substract(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiplay(int a, int b)
+        {
+            return a * b;
+        }
+        public static double Divide(int a, int b)
+        {
+            if (b != 0)
+            {
+                return (double)a / (double)b;
+            }
+            else
+            {
+                throw new DivideByZeroException("Error! Division by Zero.");
+            }
+        }
+    }
+}
---
+++ src/TeachMeSkills.DotNet.Task19/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task19
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a : ");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter b : ");
+            int b = Int32.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("-------");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine("- Addition -");
+            Console.WriteLine($"{nameof(a)} + {nameof(b)} = {Calculator.Add(a, b)}");
+            Console.WriteLine();
+            Console.WriteLine("- Substraction -");
+            Console.WriteLine($"{nameof(a)} - {nameof(b)} = {Calculator.Substract(a, b)}");
+            Console.WriteLine();
+            Console.WriteLine("- Multiplay -");
+            Console.WriteLine($"{nameof(a)} * {nameof(b)} = {Calculator.Multiplay(a, b)}");
+            Console.WriteLine();
+            Console.WriteLine("- Division -");
+            try
+            {
+                Console.WriteLine("{0:f4} / {1:f4} = {2:f2}", nameof(a), nameof(b), Calculator.Divide(a, b));
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
