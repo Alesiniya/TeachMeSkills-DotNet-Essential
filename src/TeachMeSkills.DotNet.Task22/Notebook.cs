41921e912a6f0455b51fa6efc62c2a42eb5c52b0
VicentNedelko <glory.jly@gmail.com>
2020-10-12 18:03:11 +0300

    feat: add solution for Task 22

---
+++ src/TeachMeSkills.DotNet.Task22/Notebook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeachMeSkills.DotNet.Task22
+{
+    public struct Notebook
+    {
+        public string model;
+        public string producer;
+        public decimal price;
+
+        public Notebook(string model, string producer, decimal price)
+        {
+            this.model = model;
+            this.producer = producer;
+            this.price = price;
+        }
+
+        public void PrintNotebook()
+        {
+            Console.WriteLine("Notebook parameters : ");
+            Console.WriteLine($"Model - {model}, Producer - {producer}, Price - {price};");
+        }
+    }
+}
---
+++ src/TeachMeSkills.DotNet.Task22/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter parameters of Notebook : ");
+            Console.Write("Enter Model : ");
+            string model = Console.ReadLine();
+            Console.Write("Enter producer : ");
+            string producer = Console.ReadLine();
+            Console.Write("Enter price : ");
+            decimal price = Decimal.Parse(Console.ReadLine());
+            ///<summary>
+            /// Create new instances.
+            /// </summary>
+            Notebook notebook = new Notebook();
+            Notebook notebook1 = new Notebook(model, producer, price);
+            ///<summary>
+            ///Print instances. Why does default constructor works ?
+            /// </summary>
+            notebook.PrintNotebook();
+            notebook1.PrintNotebook();
+        }
+    }
+}
