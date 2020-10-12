80d43cb733f1bc010f86200f1dba0be239e8aebc
VicentNedelko <glory.jly@gmail.com>
2020-10-12 17:35:23 +0300

    feat: add solution for Task 21

---
+++ src/TeachMeSkills.DotNet.Task21/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            myClass MyClass = new myClass();
+            myStruct MyStruct = new myStruct();
+            Console.WriteLine("-------");
+            MyClass.change = "source";
+            MyStruct.change = "source";
+            ClassTaker(MyClass);
+            StructTaker(MyStruct);
+            Console.WriteLine(MyClass.change);
+            Console.WriteLine(MyStruct.change);
+        }
+        static void ClassTaker(myClass MyClass)
+        {
+            MyClass.change = "changed";
+        }
+        static void StructTaker(myStruct MyStruct)
+        {
+            MyStruct.change = "changed";
+        }
+    }
+}
---
+++ src/TeachMeSkills.DotNet.Task21/myStruct.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeachMeSkills.DotNet.Task21
+{
+    public struct myStruct
+    {
+        public string change;
+    }
+}
