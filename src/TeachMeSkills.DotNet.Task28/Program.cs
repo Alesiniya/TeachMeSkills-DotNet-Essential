e2be14ed039cffe64b2860d83cdff5ae6b06994e
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-10-17 22:16:02 +0300

    feat: add solution for Task 28;

---
+++ src/TeachMeSkills.DotNet.Task28/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int, double> getAverageValue = delegate (int val1, int val2, int val3)
+            {
+                return (double)(val1 + val2 + val3) / 3.0;
+            };
+            Console.WriteLine($"{getAverageValue.Invoke(2, 2, 3):f2}");
+        }
+    }
+}
