e709100a26439ec7ee4265078eab5a35228ec1ab
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-10-17 21:58:01 +0300

    feat: add solution for task 27;

---
+++ src/TeachMeSkills.DotNet.Task27/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task27
+{
+    class Program
+    {
+        delegate int GenerateRandom();
+        delegate double GetAverage(GenerateRandom[] generateRans);
+        static void Main(string[] args)
+        {
+            GenerateRandom[] arrOfDel = new GenerateRandom[10];
+            GetAverage getAverage = delegate (GenerateRandom[] generateRans)
+            {
+                int sum = 0;
+                for(int i = 0; i < generateRans.Length; i++)
+                {
+                    sum += generateRans[i].Invoke();
+                }
+                return (double)sum / (double)generateRans.Length;
+            };
+            GenerateRandom generator = delegate ()
+            {
+                Random rnd = new Random();
+                return rnd.Next(100);
+            };
+            for (int j = 0; j < 10; j++)
+            {
+                arrOfDel[j] = generator;
+            };
+            Console.WriteLine($"Result - {getAverage(arrOfDel):f2}");
+        }
+    }
+}
